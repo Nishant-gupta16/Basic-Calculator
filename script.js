164d66169b589b11b7a2920afd2823210b708a8a
Nishant-gupta16 <168644246+Nishant-gupta16@users.noreply.github.com>
2025-11-27 19:47:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("display");
+
+function pressValue(num) {
+    display.value += num;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
